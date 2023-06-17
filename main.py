edf11be511f4b19190ad443f892d97f9e7b9f1e5
Matthev00 <mateuszostaszewskififa@gmail.com>
2023-06-17 18:18:19 +0200

    loaded best model

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import torch
-from torchvision import transforms
 
 import data_setup
 import utils
 import model_builder
-import engine
+import engine # noqa 5501
 
 import os
 from pathlib import Path
@@ -22,21 +21,19 @@ def main():
     data_dir = Path("data/10_percent_data")
     train_dir = data_dir / "train"
     test_dir = data_dir / "test"
-    simple_transform = transforms.Compose([transforms.Resize((224, 224)),
-                                           transforms.ToTensor()])
-
-    train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders( # noqa 5501
-        train_dir=train_dir,
-        test_dir=test_dir,
-        train_transforms=simple_transform,
-        test_transforms=simple_transform,
-        num_workers=NUM_WORKERS,
-        batch_size=BATCH_SIZE
-    )
 
     model, model_transform = model_builder.create_densenet(
         num_classes=2,
         device=device
+    )
+
+    train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders( # noqa 5501
+        train_dir=train_dir,
+        test_dir=test_dir,
+        train_transforms=model_transform,
+        test_transforms=model_transform,
+        num_workers=NUM_WORKERS,
+        batch_size=BATCH_SIZE
     )
 
     imgs = list(test_dir.glob("*/*.jpeg"))
@@ -44,30 +41,33 @@ def main():
     img = Image.open(img_path)
     transformed_img = model_transform(img).unsqueeze(dim=0)
 
+    best_model_path = "models/Pretrained_densenet_20%_data_10_epochs.pth"
+    model.load_state_dict(torch.load(f=best_model_path))
+
     model.eval()
     with torch.inference_mode():
         img_pred = model(transformed_img.to(device))
     label = torch.argmax(torch.softmax(img_pred, 1), 1)
     print(class_names[label])
 
-    # utils.pred_and_plot_image(model=model,
-    #                           image_path=img_path,
-    #                           class_names=class_names,
-    #                           transform=model_transform)
+    utils.pred_and_plot_image(model=model,
+                              image_path=img_path,
+                              class_names=class_names,
+                              transform=model_transform)
     # utils.print_summary(model)
 
-    loss_fn = torch.nn.CrossEntropyLoss(label_smoothing=0.1)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
-    results = engine.train(model=model,
-                           train_dataloader=train_dataloader,
-                           test_dataloader=test_dataloader,
-                           loss_fn=loss_fn,
-                           optimizer=optimizer,
-                           writer=utils.create_writer(experiment_name="test",
-                                                      model_name="xyz")
-                           )
-    print(results)
-    utils.plot_loss_curves(results=results)
+    # loss_fn = torch.nn.CrossEntropyLoss(label_smoothing=0.1)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    # results = engine.train(model=model,
+    #                        train_dataloader=train_dataloader,
+    #                        test_dataloader=test_dataloader,
+    #                        loss_fn=loss_fn,
+    #                        optimizer=optimizer,
+    #                        writer=utils.create_writer(experiment_name="test",
+    #                                                   model_name="xyz")
+    #                        )
+    # print(results)
+    # utils.plot_loss_curves(results=results)
 
 
 if __name__ == "__main__":
